b1b44cfdc5e23ad8e43e1c23451279e8c0673b54
EduardoDev98 <eduardo.garcia.r98@gmail.com>
2024-04-25 21:24:20 -0300

    DESAFIO

---
+++ desafio1.js
@@ -0,0 +1,24 @@
+const convertButton = document.querySelector(".convert-button");
+
+function convertValues() {
+  const inputCurrencyValue = document.querySelector(".input-currency").value;
+
+  const currencyValueToConvert = document.querySelector(
+    ".currency-value-to-convert"
+  ); //valor em real
+
+  const currencyValueConverted = document.querySelector(".currency-value"); // outras moedas
+  const dolarToday = 5.2;
+  const convertedValue = inputCurrencyValue / dolarToday;
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrencyValue);
+  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(convertedValue);
+  currencyValueConverted.innerHTML = convertedValue;
+  console.log(convertedValue);
+}
+convertButton.addEventListener("click", convertValues);
