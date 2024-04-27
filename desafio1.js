5cbb5ec387dfcce7fa035113100f55e860aef390
EduardoDev98 <eduardo.garcia.r98@gmail.com>
2024-04-26 22:38:01 -0300

    atualizaçãp

--- desafio1.js
+++ desafio1.js
@@ -1,4 +1,5 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -8,17 +9,32 @@ function convertValues() {
   ); //valor em real.
 
   const currencyValueConverted = document.querySelector(".currency-value"); // outras moedas
+
+  console.log(currencySelect.value);
   const dolarToday = 5.2;
-  const convertedValue = inputCurrencyValue / dolarToday;
+  const euroToday = 6.2;
+
+  if (currencySelect.value == "dolar") {
+    // Se o select estiver selecionado o valor de dolar , entre aqui!
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrencyValue / dolarToday);
+  }
+
+  if (currencySelect.value == "euro") {
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrencyValue / euroToday);
+  }
+
   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
   }).format(inputCurrencyValue);
-  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(convertedValue);
-  currencyValueConverted.innerHTML = convertedValue;
-  console.log(convertedValue);
+
+  //currencyValueConverted.innerHTML = convertedValue; ERRO NA FORMATAÇÃO DO DOLAR !!!!!
+  // console.log(convertedValue);
 }
 convertButton.addEventListener("click", convertValues);
